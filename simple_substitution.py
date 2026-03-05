dc5efb5c17ecd6ab865f962cb06fde0aaf7c0ed0
alessandrine <alvinorkina@edu.hse.ru>
2026-03-05 07:59:37 +0300

    file handling

--- simple_substitution.py
+++ simple_substitution.py
@@ -19,10 +19,24 @@ def simple_substitution(alphabet, text, key, type_e_d='e'):
 
 
 alph_simple = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-text_simple = input(
-    "Введите в качестве обрабатываемого текста последовательность латинских символов в верхнем регистре: ")
-key_simple = input(
-    "Введите в качестве ключа последовательность, являющейся перестановкой символов латинского алфавита в верхнем регистре: ")
-type_e_d_simple = input(
-    "Выберите режим: чтобы произвести зашифрование (encryption), введите «e» без кавычек; для расшифрования (decryption) – «d» аналогично: ")
+flag_files = input("Работа с файлами (y) или ручной ввод (n)?")
+if flag_files == 'y':
+    inp_simple = input("Файл с исходным текстом: ")
+    with open(f"{inp_simple}", 'r', encoding='utf-8') as inp_file:
+        text_simple = inp_file.readline()
+    key_simple = input(
+        "Введите в качестве ключа последовательность, являющейся перестановкой символов латинского алфавита в верхнем регистре: ")
+    type_e_d_simple = input(
+        "Выберите режим: чтобы произвести зашифрование (encryption), введите «e» без кавычек; для расшифрования (decryption) – «d» аналогично: ")
+    out_simple = input("Файл для полученного в ходе преобразований текста: ")
+    out_text_simple = simple_substitution(alph_simple, text_simple, key_simple, type_e_d_simple)
+    with open(f"{out_simple}", 'w') as out_file:
+        out_file.write(out_text_simple)
+else:
+    text_simple = input(
+        "Введите в качестве обрабатываемого текста последовательность латинских символов в верхнем регистре: ")
+    key_simple = input(
+        "Введите в качестве ключа последовательность, являющейся перестановкой символов латинского алфавита в верхнем регистре: ")
+    type_e_d_simple = input(
+        "Выберите режим: чтобы произвести зашифрование (encryption), введите «e» без кавычек; для расшифрования (decryption) – «d» аналогично: ")
 print(simple_substitution(alph_simple, text_simple, key_simple, type_e_d_simple))
