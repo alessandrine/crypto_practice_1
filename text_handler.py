e0cbf22641bcfaa7df64a2573548ad2e6de94ccc
alessandrine <alvinorkina@edu.hse.ru>
2026-03-05 07:25:02 +0300

    handling the spaces again

--- text_handler.py
+++ text_handler.py
@@ -4,7 +4,7 @@ with open(f"{file_input}", 'r', encoding='utf-8') as text_file:
     text = text_file.read()
     temp = []
     for elem in text:
-        if not ((65 <= ord(elem) <= 90) or (97 <= ord(elem) <= 122)):
+        if not ((65 <= ord(elem) <= 90) or (97 <= ord(elem) <= 122) or (elem == ' ')):
             if elem not in temp:
                 temp.append(elem)
         elif 97 <= ord(elem) <= 122:
